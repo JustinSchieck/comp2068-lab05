27b447c1263a8466d52babe883b28015811b46f5
Justin Schieck <schieck91@gmail.com>
2017-03-09 23:56:47 -0500

    second commit, edited users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
+/**
+* @Author: Justin Schieck
+* @Date:   2017-03-09T23:09:15-05:00
+* @Email:  schieck91@gmail.com
+* @Last modified by:   Justin Schieck
+* @Last modified time: 2017-03-09T23:56:16-05:00
+*/
+
+
+
 var express = require('express');
 var router = express.Router();
+var Account = require('../models/account');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    //finds all users
+    Account.find(function (err, accounts) {
+      //error validation
+        if (err) {
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            res.render('users', {
+                title: 'Users',
+                user: accounts
+            });
+        }
+    });
 });
 
 module.exports = router;
