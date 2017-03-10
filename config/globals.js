d3aac82f260220a1bcf744a5c88d040ed476c7e6
Justin Schieck <schieck91@gmail.com>
2017-03-09 23:15:00 -0500

    first commit

---
+++ config/globals.js
@@ -0,0 +1,15 @@
+/**
+* @Author: Justin Schieck
+* @Date:   2017-03-08T16:00:13-05:00
+* @Email:  schieck91@gmail.com
+* @Last modified by:   Justin Schieck
+* @Last modified time: 2017-03-08T16:02:19-05:00
+*/
+
+
+
+// array of global variables
+module.exports = {
+    // db: 'mongodb://localhost/comp2068-w2017' // local mongodb
+    db: 'mongodb://JustinSchieck:Kiraanne1@ds147079.mlab.com:47079/comp2068'  // mlab
+};
